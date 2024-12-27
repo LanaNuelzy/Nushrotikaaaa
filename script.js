e57f91a8f6a85eb58139f2724c5a88a42355fbec
LanaNuelzy <maulanaburhanudinjpr@gmail.com>
2024-12-27 10:07:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("gambar").src =
+    "https://cdn.pixabay.com/photo/2013/07/12/19/02/happy-birthday-154242_960_720.png";
+  document.getElementById("tulisan").innerHTML =
+    "SELAMAT ULANG TAHUN SAYANGKUU SEMOGA PANJANG UMUR DAN SEHAT SEALU, BERSAMAA TERUSS YAAAA";
+}
